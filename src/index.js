b8a486cfeb5a63b8298f2279ef3a9e3646439b41
Onda18 <bboyonda@gmail.com>
2020-04-29 22:36:41 -0400

    LandingPage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import './css/index.css';
 import { render } from 'react-dom';
 // import 'normalize.css';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPluging = require('mini-css-extract-plugin');
 
 module.exports = {
    entry: path.join(__dirname, './src/index.js'),
@@ -17,12 +18,36 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
-         }
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: ['url-loader?limit=10000', 'img-loader'],
+          },
+         {
+            test: /\.(sa|c)ss$/i,
+            use: [
+              'style-loader',
+              MiniCssExtractPluging.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          },
       ]
    },
    plugins:[
       new HtmlWebpackPlugin({
          template: './index.html'
-      })
+      }),
+      new MiniCssExtractPluging()
    ]
 }

34e35b61721837d092f84bb6ed1be086c33745d8
Onda18 <bboyonda@gmail.com>
2020-05-01 14:08:15 -0400

    Ligeros Cambios

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import logo from '../logo.png'
 
 const App = () => {
     return (
-        <div className = "container">
+        <>
+            <div className = "banner">
 
-            <div className = "header">
-                <img src = {logo} alt = "Logo" />
-                <nav>
-                    <a>Home</a>
-                    <a>Search</a>
-                    <a>Random</a>
-                </nav>
+                <div className = "container">
+                    <div className = "header">
 
+                        <div className = "pokemon-logo">
+                            <img src = {logo} alt = "Logo" />
+                        </div>
+
+                        <nav className = "nav-bar">
+                            <a> <i className="fas fa-home"></i>Home</a>
+                            <a> <i class="fas fa-search"></i>Search</a>
+                            <a> <i className="fas fa-question-circle"></i>Random</a>
+                        </nav>
+                    </div>
+                </div>
+                
             </div>
 
-            <div className = "desc">
-            <h1>Current Title</h1>
-            <p>
-            - Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-            </p>
+            <div className = "main">
+
+                <div className = "desc container">
+                    <h1>Current Title</h1>
+                    <p>
+                    - Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+                    </p>
+                </div>
+
             </div>
-        </div>
+        </>
     )
 }
 
